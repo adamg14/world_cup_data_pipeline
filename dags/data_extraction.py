56dda49f416ca6086a32e73c681340b40b95c4be
Adam Worede <adamg2609@gmail.com>
2025-05-27 23:48:44 +0100

    data load task added

--- dags/data_extraction.py
+++ dags/data_extraction.py
@@ -1,13 +1,13 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
-from pipelines.data_pipeline import extract_dataset, read_csv
+from pipelines.data_pipeline import extract_dataset, read_csv, write_data_lake
 
 dag = DAG(
     dag_id="web_scraping",
+    start_date = datetime(2025, 5, 27),
     default_args = {
-        "owner": "Adam Worede",
-        "start_date": datetime(2025, 5, 25),
+        "owner": "Adam Worede"
     },
     schedule_interval = None,
     catchup = False
@@ -31,8 +31,9 @@ load_stadium_data = PythonOperator(
     provide_context = True,
     op_kwargs = {
         "local_file_path": "./data/stadiums.csv",
-        
-    }
+        "adl_file_path": "raw-data/stadiums.csv"
+    },
+    dag = dag
 )
 
 extract_goalscorer_data = PythonOperator(
@@ -76,5 +77,7 @@ extract_match_data = PythonOperator(
         "destination_directory": "./data"
     }
 )
+
+extract_stadium_data >> load_stadium_data
 
 # data loading
--- dags/pipelines/data_pipeline.py
+++ dags/pipelines/data_pipeline.py
@@ -7,10 +7,19 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../../.env"))
 
+account_name = os.getenv("STORAGE_ACCOUNT_NAME")
+account_key = os.getenv("STORAGE_ACCOUNT_KEY")
+
 # azure data lake file system
 fs = AzureBlobFileSystem(
+    account_name=account_name,
+    account_key=account_key
+)
+
+def get_azure_fs():
+    return AzureBlobFileSystem(
     account_name=os.getenv("STORAGE_ACCOUNT_NAME"),
-        account_key=os.getenv("STORAGE_ACCOUNT_KEY")
+    account_key=os.getenv("STORAGE_ACCOUNT_KEY"),
 )
 
 def extract_dataset(url, destination_directory):
@@ -36,8 +45,9 @@ def read_csv(file_path):
 
 
 def write_data_lake(local_file_path, adl_file_path):
+    fs = get_azure_fs()
     with open(local_file_path, "rb") as data_source:
         with fs.open(adl_file_path, 'wb') as data_lake:
             data_lake.write(data_source.read())
 
-write_data_lake("data/stadiums.csv", "/raw-data/stadiums.csv")
+# write_data_lake("data/stadiums.csv", "/raw-data/stadiums.csv")
